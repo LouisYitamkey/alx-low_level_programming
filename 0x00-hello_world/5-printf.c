35f7ebfcd3b2aac890b77f2099330d4ab7213374
LouisYitamkey <bonglamgh@gmail.com>
2023-07-13 11:33:33 -0700

    Solution to c hello wolrd question 3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 /**
- * main- entry point
- * return always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
